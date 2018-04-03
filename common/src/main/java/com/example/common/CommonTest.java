b172c81e0c133f3734956b4daab0c551d9ab0ade
theneims <35737957+theneims@users.noreply.github.com>
2018-04-03 14:35:12 -0400

    Created CommonTest.java to test other classes in common folder.

---
+++ common/src/main/java/com/example/common/CommonTest.java
@@ -0,0 +1,7 @@
+package com.example.common;
+
+public class CommonTest {
+    public static void main() {
+        // TODO test User.java, Class.java, Quiz.java, Question.java, QuestionImage.java, and Answer.java
+    }
+}
