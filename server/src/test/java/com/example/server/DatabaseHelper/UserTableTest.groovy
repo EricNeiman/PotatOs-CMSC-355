a9c815e41ecbc68b67830403f346a9120af0f269
Jacob Draddy <jakedraddy@users.noreply.github.com>
2018-04-04 18:06:36 -0400

    Unit tests and quiz create sql

---
+++ server/src/test/java/com/example/server/DatabaseHelper/UserTableTest.groovy
@@ -0,0 +1,15 @@
+package com.example.server.DatabaseHelper
+
+class UserTableTest extends GroovyTestCase {
+    void testGetUserById() {
+    }
+
+    void testGetUserByEmailPass() {
+    }
+
+    void testUpdateUser() {
+    }
+
+    void testCreateUser() {
+    }
+}
