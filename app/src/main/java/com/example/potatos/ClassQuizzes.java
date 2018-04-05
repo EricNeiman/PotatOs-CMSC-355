1f1f9a7f57ee7d6e9815eba7f4fd3c3b50942e2a
Mamadou Diallo <diallomc@vcu.edu>
2018-04-04 22:33:38 -0400

    fixed my earlier problem

--- app/src/main/java/com/example/potatos/ClassQuizzes.java
+++ app/src/main/java/com/example/potatos/ClassQuizzes.java
@@ -1,7 +1,9 @@
 package com.example.potatos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ClassQuizzes extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class ClassQuizzes extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_class_quizzes);
+    }
+
+    public  void onCreateQuizclick(View v) {
+        if(v.getId() == R.id.BcreateQuiz) {
+
+
+            Intent i = new Intent(this, QuizInfo.class);
+            startActivity(i);
+        }
+
     }
 }
