7346901313c5bc9019d060eb1858dd47be1e3444
Mamadou Diallo <diallomc@vcu.edu>
2018-04-14 23:54:06 -0400

    committing to be able to merge with the branch

---
+++ app/src/main/java/com/example/potatos/AddAnswer.java
@@ -0,0 +1,14 @@
+package com.example.potatos;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddAnswer extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.addanswer);
+    }
+
+
+}
