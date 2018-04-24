2c14c532f7286a4c405f61de6ba670e13a389a18
Patrick Sullivan <pdvortex@gmail.com>
2018-04-24 15:06:19 -0400

    Fixed Teacher Overview and Proper Transition to TeacherOverview

--- app/src/main/java/com/example/potatos/MainActivity.java
+++ app/src/main/java/com/example/potatos/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.potatos;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.example.common.REST.PotatOsApi;
+import com.example.common.User;
+import com.google.gson.Gson;
+
+import static com.example.common.REST.UserREST.getByEmailPass;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,17 +24,38 @@ public class MainActivity extends AppCompatActivity {
         logInBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText usernameEditText = findViewById(R.id.usernameEditText);
+                EditText usernameEditText = findViewById(R.id.emailEditText);
                 EditText passwordEditText = findViewById(R.id.passwordEditText);
-                String username = usernameEditText.getText().toString();
+                String email = usernameEditText.getText().toString();
                 String password = passwordEditText.getText().toString();
-                /*Need to do in future Sprint
-                    Create Account Verification and judge if student or Teacher account.
-                 */
-                //This goes to the Teacher's Overview Menu for Sprint1.
-                Intent overviewTeacher = new Intent(getApplicationContext(), TeacherOverview.class);
-                overviewTeacher.putExtra("com.example.potatoes.logIn", username);
-                startActivity(overviewTeacher);
+                User user = getByEmailPass(email, password);
+
+                if (user != null) {
+                    if (user.getIsTeacher()){
+                        Gson gson = new Gson();
+                        String requestJson = gson.toJson(user);
+
+                        Intent overviewTeacher = new Intent(getApplicationContext(), TeacherOverview.class);
+                        overviewTeacher.putExtra("com.example.potatoes.logIn", requestJson);
+                        startActivity(overviewTeacher);
+                    }
+                    else {
+                        Gson gson = new Gson();
+                        String requestJson = gson.toJson(user);
+
+                        Intent overviewTeacher = new Intent(getApplicationContext(), TeacherOverview.class);
+                        overviewTeacher.putExtra("com.example.potatoes.logIn", user);
+                        startActivity(overviewTeacher);
+                    }
+                }
+            }
+        });
+
+        Button newAccountBtn = findViewById(R.id.newAccountBtn);
+        newAccountBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
             }
         });
     }
